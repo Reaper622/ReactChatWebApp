86d6ff7805725c6d1fb411b2efd5be1f508a8459
libowen <1064416560@qq.com>
2019-03-15 09:00:14 +0800

    :zap: feat: 增加了未读消息数展示

--- src/component/msg/msg.js
+++ src/component/msg/msg.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { connect } from 'react-redux'
-import {List} from 'antd-mobile'
+import {List,Badge} from 'antd-mobile'
 
 @connect(
   state => state
@@ -30,12 +30,14 @@ class Msg extends React.Component{
             console.log(v)
             const lastItem = this.getLast(v)
             const targetId = v[0].from === userid ? v[0].to : v[0].form
+            const unreadNum = v.filter(v => !v.read && v.to === userid).length
             if(!userinfo[targetId]) {
               return null
             }
             return (
               <List key={lastItem._id}>
                 <Item
+                  extra={<Badge text={unreadNum}></Badge>}
                   thumb={require(`../img/${userinfo[targetId].avatar}.png`)}
                   >
                   {lastItem.content}
